617a916186d48a40554451e8c663877f411646a7
jessijoke <jessijoke@yahoo.com>
2021-04-03 17:46:05 -0400

    Additional styling, validations and errors displaying on signup, user info displayed in top bar on login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,25 @@ class UsersController < ApplicationController
   
     def create
       @user = User.create(user_params)
-      return redirect_to controller: 'users', action: 'new' unless @user.save
-      session[:user_id] = @user.id
-      redirect_to controller: 'pages', action: 'index'
+      if @user.valid?
+        @user.save
+        session[:user_id] = @user.id
+        redirect_to controller: 'pages', action: 'index'
+      else
+        flash.now[:messages] = @user.errors.full_messages[0]
+        render controller: 'users', action: 'new'
+        #redirect_to controller: 'users', action: 'new'
+      end
+
+
+    #   return redirect_to controller: 'users', action: 'new' unless @user.save
+    #   session[:user_id] = @user.id
+    #   redirect_to controller: 'pages', action: 'index'
     end
   
     private
   
     def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :password, confirmation: true
+    validates :name, presence: true, :uniqueness => {:scope => :name}
+    validates :email, presence: true, :uniqueness => {:scope => :email}
 end
