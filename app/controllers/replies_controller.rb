b939029f6916f12993807751f82212414e932e82
jessijoke <jessijoke@yahoo.com>
2021-04-22 23:24:56 -0400

    Forum fix
    uh

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -4,11 +4,10 @@ class RepliesController < ApplicationController
     end
 
     def create
+        byebug
         @post = Post.find(params["post_id"].keys[0].to_i)
         @reply = Reply.create(message: params["message"], user_id: current_user.id, post_id: @post.id)
-        if @reply.user.id != current_user.id
-            render post_path(@post)
-        end
+        redirect_to post_path(@post)
     end
 
     def edit
