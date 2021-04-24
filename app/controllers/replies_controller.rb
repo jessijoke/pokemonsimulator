32383a8a071f9666c59a5db173dfc9a756961728
jessijoke <jessijoke@yahoo.com>
2021-04-24 14:26:31 -0400

    fixing forum glitch

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -4,8 +4,7 @@ class RepliesController < ApplicationController
     end
 
     def create
-        byebug
-        @post = Post.find(params["post_id"].keys[0].to_i)
+        @post = Post.find(params["post_id"])
         @reply = Reply.create(message: params["message"], user_id: current_user.id, post_id: @post.id)
         redirect_to post_path(@post)
     end
